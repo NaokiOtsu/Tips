0fc63eb0573d2b0cb5f0cade4b1d2e3f7e05fce6
Naoki Otsu <n.otsu1029@gmail.com>
2014-09-06 12:15:34 +0900

    basketball added

--- BasketballCourtTokyo/app/js/main.js
+++ BasketballCourtTokyo/app/js/main.js
@@ -1,13 +1,32 @@
 ;(function(){
 	
 	var initialize = function () {
-		console.log(107);
+		var myLating = new google.maps.LatLng(35.650337, 139.689516);
+
 		var mapOptions = {
-			center: new google.maps.LatLng(35.650337, 139.689516),
+			center: myLating,
 			zoom: 18,
 			mapTypId: google.maps.MapTypeId.ROADMAP
 		};
 		var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+		
+		var icon = new google.maps.MarkerImage(
+			'./img/twitter.png',
+			new google.maps.Size(201, 61),
+			new google.maps.Point(0, 0)
+		);
+		var marker = new google.maps.Marker({
+			position: myLating,
+			icon: icon,
+			title: 'hello world'
+		});
+
+		marker.setMap(map);
+
+		// markerクリックしたら
+		google.maps.event.addListener(marker, 'mouseover', function(){
+			console.log(111);
+		});
 	};
 
 	window.addEventListener('DOMContentLoaded', function(){
