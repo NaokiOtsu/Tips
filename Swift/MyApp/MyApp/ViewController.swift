9b13a403143c5976276b682ecc168d91f29e0bdb
naoki.otsu <naoki.otsu@dena.jp>
2015-06-10 09:56:54 +0900

    Swift勉強

--- Swift/MyApp/MyApp/ViewController.swift
+++ Swift/MyApp/MyApp/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func kakusu(sender: AnyObject) {
+//        mainImage.hidden = true
+    }
 
+    @IBAction func hyouji(sender: AnyObject) {
+//        mainImage.hidden = false
+    }
 }
 
