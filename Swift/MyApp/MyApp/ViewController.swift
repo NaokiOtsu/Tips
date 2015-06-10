c91a7aee644003641d5885053e6c05a43a07614b
NaokiOtsu <n.otsu1029@gmail.com>
2015-06-10 09:24:10 +0900

    Initial Commit

---
+++ Swift/MyApp/MyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Naoki Otsu on 2015/06/10.
+//  Copyright (c) 2015年 NaokiOtsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
