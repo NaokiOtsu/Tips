c6d0d0500b67818c93f71e37c82f0e97bc5c954b
NaokiOtsu <n.otsu1029@gmail.com>
2015-06-11 09:54:06 +0900

    swift,Xcode勉強を追加

--- Swift/FortuneTelling/FortuneTelling/ViewController.swift
+++ Swift/FortuneTelling/FortuneTelling/ViewController.swift
@@ -9,7 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var animalLabel: UILabel!
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func tellFortune() {
+        let randomNum = Int(arc4random_uniform(3))
+        
+        if randomNum == 0 {
+            animalLabel.text = "🐶"
+            resultLabel.text = "大吉"
+        } else if (randomNum == 1) {
+            animalLabel.text = "🐱"
+            resultLabel.text = "中吉"
+        } else if (randomNum == 2) {
+            animalLabel.text = "🐒"
+            resultLabel.text = "小吉"
+        } else if (randomNum == 3) {
+            animalLabel.text = "🐴"
+            resultLabel.text = "末吉"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
