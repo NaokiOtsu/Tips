eab273b5f986b4e8680192dbe7963c942152f31e
naoki <n.otsu1029@gmail.com>
2015-09-02 08:35:37 +0900

    Backbone.js EBookManagerの実装

--- BackboneJsTest/EBookManager/site/js/views/book.js
+++ BackboneJsTest/EBookManager/site/js/views/book.js
@@ -2,5 +2,20 @@ var app = app || {};
 
 app.BookView = Backbone.View.extend({
 	tagName: 'div',
+	className: 'bookContainer',
+	template: _.template($('#bookTemplate').html()),
 	
+	events: {
+		'click .delete': 'deleteBook'
+	},
+	
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	},
+	
+	deleteBook: function() {
+		this.model.destroy();
+		this.remove();
+	}
 });
