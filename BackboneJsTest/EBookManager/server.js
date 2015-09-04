78492d615caf7a529dd0998ba0dd16c31d433d5d
NaokiOtsu <n.otsu1029@gmail.com>
2015-09-05 00:11:50 +0900

    Backbone.js EBookManagerの実装

--- BackboneJsTest/EBookManager/server.js
+++ BackboneJsTest/EBookManager/server.js
@@ -32,12 +32,79 @@ app.get('/api/books', function(request, response) {
 	})
 });
 
+app.get('/api/books/:id', function(request, response) {
+	return BookModel.findById(request.params.id, function(err,book){
+		if(!err) {
+			return response.send(book);
+		} else {
+			return console.log(err);
+		}
+	});
+});
+
+app.post('/api/books', function(request, response) {
+	var book = new BookModel({
+		title: request.body.title,
+		author: request.body.author,
+		releaseDate: request.body.releaseDate,
+		keywords: request.body.keywords
+	});
+	book.save(function(err) {
+		if(!err) {
+			return console.log('追加されました');
+		} else {
+			return console.log(err);
+		}
+	});
+	return response.send(book);
+});
+
+app.put('/api/books/:id', function(request, response) {
+	console.log('更新します: ' + request.body.title);
+	return BookModel.findById(request.params.id, function(err, book) {
+		book.title = request.body.title;
+		book.author = request.body.author;
+		book.releaseDate = request.body.releaseDate;
+		book.keywords = request.body.keywords;
+		
+		return book.save(function(err) {
+			if(!err) {
+				console.log('更新されました');
+			} else {
+				console.log(err);
+			}
+			return response.send(book);
+		});
+	});
+});
+
+app.delete('/api/books/:id', function(request, response) {
+	console.log('削除する本のID: ' + request.params.id);
+	return BookModel.findById(request.params.id, function(err, book) {
+		return book.remove(function(err) {
+			if(!err) {
+				console.log('本が削除されました');
+				return response.send('');
+			} else {
+				console.log(err);
+			}
+		});
+	});
+});
+
 mongoose.connect('mongodb://localhost/library_database');
+
+var BookModel = mongoose.model('Book', Book);
+
+var KeyWords = new mongoose.Schema({
+	keyword: String
+});
 
 var Book = new mongoose.Schema({
 	title: String,
 	author: String,
-	releaseDate: Date
+	releaseDate: Date,
+	keywords: [ Keywords ]
 });
 
-var BookModel = mongoose.model('Book', Book);
+
