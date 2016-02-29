d3cad9c7b5f28bf8efa0d898c1996ddda735e31e
naoki <n.otsu1029@gmail.com>
2016-03-01 07:17:27 +0900

    nodejs + backbone

---
+++ NodeJS/BackboneJS/server.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  console.log(__dirname);
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
