fed4e8b85a02aacc042cca4e6a62c0d2f3834302
NaokiOtsu <n.otsu1029@gmail.com>
2016-04-13 16:01:35 +0900

    [master] Gulp+PHPを更新

---
+++ Gulp-PHP/public/pc/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Gulp + PHP</title>
+</head>
+<body>
+	<? require('../template/header.html') ?>
+	
+	hoge hoge
+	
+	<? require('../template/footer.html') ?>
+</body>
+</html>
