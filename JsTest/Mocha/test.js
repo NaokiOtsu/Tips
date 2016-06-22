6b584263854afac2755f7ffb9c9eb8d6a8d5f6e0
NaokiOtsu <n.otsu1029@gmail.com>
2016-06-22 11:44:33 +0900

    update

---
+++ JsTest/Mocha/test.js
@@ -0,0 +1,14 @@
+describe('add関数のテスト', function() {
+    it('1+2は3である', function() {
+        if (add(1, 2) === 3) {
+        } else {
+            throw new Error('失敗');
+        }
+    });
+    it('1+2は4である', function() {
+        if (add(1, 2) === 4) {
+        } else {
+            throw new Error('失敗');
+        }
+    });
+});
