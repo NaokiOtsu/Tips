66fe73a4ef313862bb910cfe1247b9a4b284c867
NaokiOtsu <n.otsu1029@gmail.com>
2017-01-18 16:56:13 +0900

    update

---
+++ JavaScriptSample/SortTable2/JSON.js
@@ -0,0 +1,62 @@
+var DATA = [
+  {
+    id: 1,
+    name: 'name1',
+    img: '<img src="">',
+    price: 1000
+  },
+  {
+    id: 2,
+    name: 'name2',
+    img: '<img src="">',
+    price: 2000
+  },
+  {
+    id: 3,
+    name: 'name3',
+    img: '<img src="">',
+    price: 3000
+  },
+  {
+    id: 4,
+    name: 'name4',
+    img: '<img src="">',
+    price: 4000
+  },
+  {
+    id: 5,
+    name: 'name5',
+    img: '<img src="">',
+    price: 5000
+  },
+  {
+    id: 6,
+    name: 'name6',
+    img: '<img src="" onerror="alert(1)">',
+    price: 6000
+  },
+  {
+    id: 7,
+    name: 'name7',
+    img: '<img src="">',
+    price: 7000
+  },
+  {
+    id: 8,
+    name: 'name8',
+    img: '<img src="">',
+    price: 8000
+  },
+  {
+    id: 9,
+    name: 'name9',
+    img: '<img src="">',
+    price: 9000
+  },
+  {
+    id: 10,
+    name: 'name10',
+    img: '<img src="">',
+    price: 10000
+  }
+];
