06a1eead17d8d0445815e4fdfc662704cfa10896
NaokiOtsu <n.otsu1029@gmail.com>
2017-01-27 11:31:34 +0900

    update

---
+++ Redux/todo-app/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import FilterLink from '../containers/FilterLink';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <p>
+        {" "}
+        <FilterLink filter="SHOW_ALL">
+          All
+        </FilterLink>
+        {", "}
+        <FilterLink filter="SHOW_ACTIVE">
+          Active
+        </FilterLink>
+        {", "}
+        <FilterLink filter="SHOW_COMPLETED">
+          Completed
+        </FilterLink>
+      </p>
+    );
+  }
+}
+
+export default Footer;
