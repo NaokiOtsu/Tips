0bbd7a11da9eb1d578b25471be12065cb86ff40c
NaokiOtsu <n.otsu1029@gmail.com>
2017-02-01 18:10:27 +0900

    update

---
+++ JavaScriptSample/SortTable4/JSON.js
@@ -0,0 +1,62 @@
+var DATA = [
+  {
+    "id": 1,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 1
+  },
+  {
+    "id": 2,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 10
+  },
+  {
+    "id": 3,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 100
+  },
+  {
+    "id": 4,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 1000
+  },
+  {
+    "id": 5,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 10000
+  },
+  {
+    "id": 6,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 100000
+  },
+  {
+    "id": 7,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 1000000
+  },
+  {
+    "id": 8,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 10000000
+  },
+  {
+    "id": 9,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 100000000
+  },
+  {
+    "id": 10,
+    "name": "hoge",
+    "img": "<img src=\"\" onerror=\"alert(1)\">",
+    "price": 1000000000
+  }
+];
