6b6823a8c75db664c79ad21bb73a10857d882597
naoki-otsu <naoki.otsu@dena.com>
2017-02-10 15:58:48 +0900

    update

--- SPA/Vanilla/app.js
+++ SPA/Vanilla/app.js
@@ -10,11 +10,32 @@
   
   function urlChangeHandler() {
     var page_id = parseUrl(window.location.hash);
-    $pages
-      .detach()
-      .filter('.page' + page_id)
-      .appendTo('article');
-    console.log('page_id', page_id)
+    
+    var $prevPage = $pages.filter(':visible');
+    var $nextPage = $pages.filter('.page' + page_id);
+    
+    function enter() {
+      $pages.detach();
+      
+      $nextPage
+        .removeClass('page-enter')
+        .appendTo('article')
+        .addClass('page-enter');
+    }
+    
+    if ($prevPage.length > 0) {
+      $prevPage
+        .addClass('page-leave')
+        .on('webkitAnimationEnd', function onFadeOut() {
+          $nextPage
+            .off('webkitAnimationEnd', onFadeOut)
+            .removeClass('page-leave')
+            .detach();
+        });
+        enter();
+    } else {
+      enter();
+    }
   }
   
   function parseUrl(url) {
