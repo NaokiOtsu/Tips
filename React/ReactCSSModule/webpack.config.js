e08a56f1723235e51587d0ad23d3058274b87747
NaokiOtsu <n.otsu1029@gmail.com>
2017-03-13 11:26:47 +0900

    added ReactCSSModules

---
+++ React/ReactCSSModule/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: src + '/index.js',
+
+  output: {
+    path: dist,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loaders: 'style-loader!css-loader?modules!postcss-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['*', '.js']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
