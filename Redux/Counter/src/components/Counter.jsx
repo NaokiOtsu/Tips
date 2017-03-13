401c2b9d88822388a63fd60c9e863e8686e1bff2
NaokiOtsu <n.otsu1029@gmail.com>
2017-03-13 16:43:56 +0900

    added redux example Counter

---
+++ Redux/Counter/src/components/Counter.jsx
@@ -0,0 +1,46 @@
+import React, { Component, PropTypes } from 'react'
+
+class Counter extends Component {
+  incrementIfOdd() {
+    if (this.props.value % 2 !== 0) {
+      this.props.onIncrement()
+    }
+  }
+
+  incrementAsync() {
+    setTimeout(this.props.onIncrement, 1000)
+  }
+
+  render() {
+    const { value, onIncrement, onDecrement } = this.props
+    return (
+      <p>
+        Clicked: {value} times
+        {' '}
+        <button onClick={onIncrement}>
+          +
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          -
+        </button>
+        {' '}
+        <button onClick={this.incrementIfOdd.bind(this)}>
+          Increment if odd
+        </button>
+        {' '}
+        <button onClick={this.incrementAsync.bind(this)}>
+          Increment async
+        </button>
+      </p>
+    )
+  }
+}
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
