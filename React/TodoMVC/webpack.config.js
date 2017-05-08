da8bfeae8f044c978c5121806c95098009569bae
NaokiOtsu <n.otsu1029@gmail.com>
2017-05-08 15:01:07 +0900

    update

---
+++ React/TodoMVC/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: src + '/js/index.js',
+
+  output: {
+    path: dist,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' }
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
