c19e22792a174308aa9ab3736549915eadda53d0
NaokiOtsu <n.otsu1029@gmail.com>
2017-05-14 22:12:20 +0900

    update

---
+++ React/UI_Component/src/components/tabs/contents.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames';
+import style from './style.css';
+
+const Contents = props => {
+  const contentsClassNames = classNames(
+    style.contents,
+    {
+      [style.active]: props.current === props.value
+    }
+  );
+  console.log(props.current)
+  console.log(props.value)
+
+  return (
+    <div className={contentsClassNames}>
+      contents{props.value}
+    </div>
+  );
+};
+
+export default Contents;
