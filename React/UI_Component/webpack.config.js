5ac87a1c0d7e92bbb28804c4ac8e6bc81da3e43d
naoki <n.otsu1029@gmail.com>
2017-05-20 08:14:16 +0900

    update

--- React/UI_Component/webpack.config.js
+++ React/UI_Component/webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
         test: /\.css$/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader' }
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' }
         ]
       }
     ]
