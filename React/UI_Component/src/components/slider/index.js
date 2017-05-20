23b70ff691896b145062250b4b961acd3ff25b81
NaokiOtsu <n.otsu1029@gmail.com>
2017-05-20 15:47:59 +0900

    update

--- React/UI_Component/src/components/slider/index.js
+++ React/UI_Component/src/components/slider/index.js
@@ -7,16 +7,29 @@ class Slider extends Component {
   constructor() {
     super();
     this.state = {
-      current: 0
+      current: 0,
+      max: 5
     }
   }
 
   handlePrevClick() {
-    console.log(111)
+    let current = this.state.current;
+    if (current <= 0) current = this.state.max;
+
+    this.setState({
+      ...this.state,
+      current: current - 1
+    });
   }
 
   handleNextClick() {
-    console.log(222)
+    let current = this.state.current;
+    if (current >= this.state.max - 1) current = -1;
+
+    this.setState({
+      ...this.state,
+      current: current + 1
+    });
   }
 
   render() {
@@ -24,10 +37,15 @@ class Slider extends Component {
     for (let i = 0; i < 5; i++) {
       items.push(<Item key={i} index={i} />);
     }
+
+    const listStyle = {
+      'transform': `translateX(-${this.state.current * 300}px)`
+    };
+
     return (
       <div>
         <div className={style.container}>
-          <ul className={style.list}>
+          <ul className={style.list} style={listStyle}>
             {items}
           </ul>
         </div>
