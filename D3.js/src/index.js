8b2805665b9ea9ec469972b209aafe2c97595015
NaokiOtsu <n.otsu1029@gmail.com>
2017-05-22 07:00:10 +0900

    update

--- D3.js/src/index.js
+++ D3.js/src/index.js
@@ -1,5 +1,68 @@
 import * as d3 from 'd3';
 
-const data = [100, 30, 49, 200, 150];
+const data = [10, 20, 40, 60, 80, 100];
+
+const margin = {
+  top: 10,
+  left: 30,
+  bottom: 10,
+  right: 30
+};
+
+const width = 500 - margin.left - margin.right;
+const height = 300 - margin.top - margin.bottom;
+
+// X軸のスケール
+const xScale = d3.scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([0, width]);
+
+// y軸のスケール
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([height, 0]);
+
+//
+// Portrait Bar 縦の棒グラフ
+//
+const portrait_bar = d3.select('#portrait-bar');
+const svg = portrait_bar
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+svg.selectAll()
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', (d, i) => i * 30)
+  .attr('y', (d) => yScale(d))
+  .attr('width', 15)
+  .attr('height', d => height - yScale(d))
+  .attr('fill', 'red');
+
+//
+// x軸の線
+//
+const yAxis = d3.axisLeft(yScale);
+
+portrait_bar.append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+  .call(yAxis)
+
+//
+// Landscape Bar 横の棒グラフ
+//
+const landscape_bar = d3.select('#landscape-bar');
+landscape_bar.attr('width', width + margin.left + margin.right).attr('height', height + margin.top + margin.bottom);
+landscape_bar.selectAll()
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', 0)
+  .attr('y', (d, i) => i * 30)
+  .attr('width', d => xScale(d))
+  .attr('height', 15)
+  .attr('fill', 'red');
 
 
