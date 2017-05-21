cd50522d95724ffcb02e29b3c3a0804162e84ebf
NaokiOtsu <n.otsu1029@gmail.com>
2017-05-22 07:21:12 +0900

    update

--- React/UI_Component/src/components/tooltip/index.js
+++ React/UI_Component/src/components/tooltip/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import style from './style.css';
 
-const ToolTip = () => (
-  <div>tooltip</div>
-);
+class ToolTip extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      active: false
+    };
+  }
+
+  handleMouseEnter() {
+    this.setState({
+      active: true
+    });
+  }
+
+  handleMouseLeave() {
+    this.setState({
+      active: false
+    });
+  }
+
+  render() {
+
+    const tooltipClassNames = classNames(
+      style.tooltip,
+      {
+        [style.active]: this.state.active
+      }
+    );
+
+    return (
+      <div 
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+        className={style.container}
+      >
+        tooltip
+        <span className={tooltipClassNames}>hogee</span>
+      </div>
+    );
+  }
+}
 
 export default ToolTip;
