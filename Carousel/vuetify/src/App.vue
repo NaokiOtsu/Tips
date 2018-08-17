d3931e0d397f1d911ff581d3f17c19963d60db00
NaokiOtsu <n.otsu1029@gmail.com>
2018-08-18 08:56:55 +0900

    update

---
+++ Carousel/vuetify/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <v-carousel>
+      <v-carousel-item>1</v-carousel-item>
+      <v-carousel-item>2</v-carousel-item>
+      <v-carousel-item>3</v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  },
+  data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+}
+</script>
+
+<style>
+@import '~vuetify/dist/vuetify.min.css';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
