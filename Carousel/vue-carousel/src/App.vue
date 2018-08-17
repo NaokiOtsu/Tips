46acc50e3c8ef609e5f7427d92d92ebed6943f1b
NaokiOtsu <n.otsu1029@gmail.com>
2018-08-18 08:17:51 +0900

    update

---
+++ Carousel/vue-carousel/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    hoge
+    <carousel :per-page="1" :loop="true" :navigation-enabled="true">
+      <slide>
+        Slide 1 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+  name: 'app',
+  components: {
+    Carousel,
+    Slide
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
