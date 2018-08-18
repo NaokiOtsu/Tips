d2a2d1439cbf205bdbc870df8d9f0d74ce9f3333
NaokiOtsu <n.otsu1029@gmail.com>
2018-08-18 09:37:06 +0900

    update

---
+++ Carousel/vue-slick/src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="app">
+    <slick
+      ref="slick"
+      :options="slickOptions"
+      @afterChange="handleAfterChange"
+      @beforeChange="handleBeforeChange"
+      @breakpoint="handleBreakpoint"
+      @destroy="handleDestroy"
+      @edge="handleEdge"
+      @init="handleInit"
+      @reInit="handleReInit"
+      @setPosition="handleSetPosition"
+      @swipe="handleSwipe"
+      @lazyLoaded="handleLazyLoaded"
+      @lazyLoadError="handleLazeLoadError">
+      <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a>
+      <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a>
+      <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a>
+      <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a>
+      <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a>
+    </slick>
+  </div>
+</template>
+
+<script>
+import Slick from 'vue-slick';
+
+export default {
+  name: 'app',
+  components: {
+    Slick
+  },
+  data() {
+        return {
+            slickOptions: {
+                slidesToShow: 3,
+                // Any other options that can be got from plugin documentation
+            },
+        };
+    },
+  methods: {
+    next() {
+            this.$refs.slick.next();
+        },
+
+        prev() {
+            this.$refs.slick.prev();
+        },
+
+        reInit() {
+            // Helpful if you have to deal with v-for to update dynamic lists
+            this.$nextTick(() => {
+                this.$refs.slick.reSlick();
+            });
+        },
+
+        // Events listeners
+        handleAfterChange(event, slick, currentSlide) {
+            console.log('handleAfterChange', event, slick, currentSlide);
+        },
+        handleBeforeChange(event, slick, currentSlide, nextSlide) {
+            console.log('handleBeforeChange', event, slick, currentSlide, nextSlide);
+        },
+        handleBreakpoint(event, slick, breakpoint) {
+            console.log('handleBreakpoint', event, slick, breakpoint);
+        },
+        handleDestroy(event, slick) {
+            console.log('handleDestroy', event, slick);
+        },
+        handleEdge(event, slick, direction) {
+            console.log('handleEdge', event, slick, direction);
+        },
+        handleInit(event, slick) {
+            console.log('handleInit', event, slick);
+        },
+        handleReInit(event, slick) {
+            console.log('handleReInit', event, slick);
+        },
+        handleSetPosition(event, slick) {
+            console.log('handleSetPosition', event, slick);
+        },
+        handleSwipe(event, slick, direction) {
+            console.log('handleSwipe', event, slick, direction);
+        },
+        handleLazyLoaded(event, slick, image, imageSource) {
+            console.log('handleLazyLoaded', event, slick, image, imageSource);
+        },
+        handleLazeLoadError(event, slick, image, imageSource) {
+            console.log('handleLazeLoadError', event, slick, image, imageSource);
+        },
+  }
+}
+</script>
+
+<style>
+/* import ''; */
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
